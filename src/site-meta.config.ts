50efdff5592a0701e7885a4c49203bcd7236cef8
Fernando <fwzk777@gmail.com>
2023-04-01 15:35:58 -0400

    primer commit

---
+++ src/site-meta.config.ts
@@ -0,0 +1,12 @@
+const siteMeta = {
+	title: "@lfwzk",
+	description:
+		"Learning Engineer at @platzi - Microsoft MVP - Lead at Developer Circles from Meta, #JavaScript #React #Web3 #BTC #ETH - #Twitch Partner 🇲🇽 🇨🇴",
+	lang: "es-ES",
+	ogLocale: "es_ES",
+	githubUrl: "https://github.com/lfwzk",
+	themeColorLight: "#fafafa",
+	themeColorDark: "#1d1f21",
+};
+
+export default siteMeta;
